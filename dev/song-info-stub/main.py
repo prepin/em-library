770337dc12f93561c31a597a2eaf52319012ed5c
Paul Repin <paul.repin@gmail.com>
2025-03-07 19:48:43 +0300

    Update stub service

--- dev/song-info-stub/main.py
+++ dev/song-info-stub/main.py
@@ -13,7 +13,7 @@ def ok(song: str | None = None, group: str | None = None):
 
     return {
         "releaseDate": "16.07.2006",
-        "text": "Ooh baby, don't you know I suffer?\nOoh baby, can you hear me moan?\nYou caught me under false pretenses\nHow long before you let me go?\n\nOoh\nYou set my soul alight\nOoh\nYou set my soul alight",
+        "text": r"Ooh baby, don't you know I suffer?\nOoh baby, can you hear me moan?\nYou caught me under false pretenses\nHow long before you let me go?\n\nOoh\nYou set my soul alight\nOoh\nYou set my soul alight",
         "link": "https://www.youtube.com/watch?v=Xsp3_a-PMTw",
     }
 
